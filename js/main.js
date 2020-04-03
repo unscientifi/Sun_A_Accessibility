9f4a87f7fb6864515125faaca35eb511f14adc0d
unscientifi <a_sun69708@fanshaweonline.ca>
2020-04-02 22:22:20 -0400

    audio text

--- js/main.js
+++ js/main.js
@@ -1,59 +1,16 @@
-// var playPauseBtn = document.querySelector('.playpause');
-// var stopBtn = document.querySelector('.stop');
-// var rwdBtn = document.querySelector('.rwd');
-// var fwdBtn = document.querySelector('.fwd');
-// var timeLabel = document.querySelector('.time');
-// var player = document.querySelector('video');
+var audioContent = document.querySelector('#audioSrc')
 
-// player.removeAttribute('controls');
 
-// playPauseBtn.onclick = function() {
-//     if(player.paused) {
-//       player.play();
-//       playPauseBtn.textContent = 'Pause';
-//     } else {
-//       player.pause();
-//       playPauseBtn.textContent = 'Play';
-//     }
-//   };
-
-//   stopBtn.onclick = function() {
-//     player.pause();
-//     player.currentTime = 0;
-//     playPauseBtn.textContent = 'Play';
-//   };
-
-//   rwdBtn.onclick = function() {
-//     player.currentTime -= 3;
-//   };
+function revealText() {
+    document.querySelector('.audioText').style.display='block';
+    //autoplay when click the reveal text button
+    audioContent.play();
+}
   
-//   fwdBtn.onclick = function() {
-//     player.currentTime += 3;
-//     if(player.currentTime >= player.duration || player.paused) {
-//       player.pause();
-//       player.currentTime = 0;
-//       playPauseBtn.textContent = 'Play';
-//     }
-//   };
+function play() {
+    audioContent.play();
+}
 
-//   player.ontimeupdate = function() {
-//     var minutes = Math.floor(player.currentTime / 60);
-//     var seconds = Math.floor(player.currentTime - minutes * 60);
-//     var minuteValue;
-//     var secondValue;
-  
-//     if (minutes<10) {
-//       minuteValue = "0" + minutes;
-//     } else {
-//       minuteValue = minutes;
-//     }
-  
-//     if (seconds<10) {
-//       secondValue = "0" + seconds;
-//     } else {
-//       secondValue = seconds;
-//     }
-  
-//     mediaTime = minuteValue + ":" + secondValue;
-//     timeLabel.textContent = mediaTime;
-//   };
+function pause() {
+    audioContent.pause();
+}
